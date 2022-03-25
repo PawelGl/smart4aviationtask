0ab5ef635ee9ada35b066b0cb1fedac5631b30c5
PawelGl <pawel.glowacz94@gmail.com>
2022-03-26 00:16:23 +0100

    refactor

--- src/main/java/com/example/smart4aviationtask/query/FlightController.java
+++ src/main/java/com/example/smart4aviationtask/query/FlightController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
-@RequestMapping("airport")
+@RequestMapping("flight")
 @RequiredArgsConstructor
 public class FlightController {
 
